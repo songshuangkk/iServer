ccc8a255aaea543d71bd38fca9f752893766ea71
shuang.song <shuang.song@tqmall.com>
2015-08-09 16:21:47 +0800

    测试使用搜索按钮进行触发

---
+++ routes/iServer/iServer.js
@@ -0,0 +1,7 @@
+/**
+ * Created by songshuang on 15/8/9.
+ */
+
+exports.search = function () {
+    console.log('user is used to search iServer!');
+};
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var iserver = require('./iServer/iServer');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/iServer', function (req, res, next) {
+  res.render('iServer/iServer');
+});
+
+router.get('/iServer/search', function (req, res, next) {
+  iserver.search();
   res.render('iServer/iServer');
 });
 

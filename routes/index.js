2c7a60cf18dc175637f7cb1c344d6092e6de43e3
shuang.song <shuang.song@tqmall.com>
2015-08-09 14:59:32 +0800

    页面的公共header添加

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/iServer', function (req, res, next) {
+  res.render('iServer/iServer');
+});
+
 module.exports = router;

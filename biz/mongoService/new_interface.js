3e78a883d3c2bdcec0274f9b58bf17c1fa066760
shuang.song <shuang.song@tqmall.com>
2015-08-20 22:47:51 +0800

    添加mongoose的模块

---
+++ biz/mongoService/new_interface.js
@@ -0,0 +1,11 @@
+/**
+ * Created by songshuang on 15/8/20.
+ */
+
+var mongoose            = require('mongoose');
+var Interface_Mondel    = mongoose.model('Interface');
+
+
+exports.insert_interface = function() {
+
+};

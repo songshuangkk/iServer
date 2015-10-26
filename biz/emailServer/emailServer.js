1f63dd4e1ca5a1f560cf9d357749aa63742fb0f2
shuang.song <shuang.song@tqmall.com>
2015-10-26 10:29:30 +0800

    用户注册认证邮箱服务

--- biz/emailServer/emailServer.js
+++ biz/emailServer/emailServer.js
@@ -1,21 +1,41 @@
 /**
  * Created by songshuang on 15/10/25.
  */
-
+var nodemailer = require('nodemailer');
 
 (function (){
     "use strict";
 
-    function init () {
+    var transport;
 
+    function init () {
+        transport = nodemailer.createTransport({
+            server: '',
+            auth: {
+                user: '',
+                pass: ''
+            }
+        });
     }
 
     var emailServer = function () {
         init();
     };
 
-    emailServer.prototype.sendCheckEmail = function (emailAddress) {
-
+    emailServer.prototype.sendCheckEmail = function (emailAddress, message) {
+        transport.send({
+            from: '',
+            to: emailAddress,
+            subject: 'check',
+            text: 'check',
+            html: message
+        }, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Send Message: ' + info.response);
+            }
+        });
     };
 
     exports = module.exports = new emailServer();
